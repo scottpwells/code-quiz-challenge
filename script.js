535726882475fbd773d03bc5071fa6a9036c88a5
scottpwells <scottpwells@gmail.com>
2020-09-29 23:02:21 -0400

    added timer interval, and if else function

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ var startQuiz = document.getElementById("start quiz");
 var answers = document.getElementById("answers");
 var selection = document.getElementById("selection");
 
+var sec = 60;
+
 var Q = 0;
 var listQuestions = [
   {
@@ -23,12 +25,19 @@ var listQuestions = [
     question:"arrays in JavaScript can be used to store_______.",
     listAnswers:["numbers and strings", "other arrays", "booleans","all of the above"],
     answer:"all of the above"
-    
- }
+},
+{
+ question: "string values must be enclosed with in____when being assigned to variables",
+ answer: ["commas", "curly brackets","quotes","parentheses",]
+},
+{
+    question: "a very useful tool used during development and debugging for printing content to the debugger is:",
+    answer: ["javasScript", " terminal/bash", "for loops", "console.log",]
+},
+
 ]
 /**
  * 
-
  * Takes in whatever array we pass in.
  * Preferably options for the current stage. 
  */
@@ -61,24 +70,36 @@ function renderOptions() {
 //     button.setAttribute("data-value", array[i]);
     // 3. Append to an existing element
     // options.append(button);
-  }
-
+    
+}
 function questionClick() {
-    console.log(this.value)
-    // if statement for right or wrong answers
-    if(currentQuestion.answer !== this.value) {
-        //reduce time
+
+    if(currentQuestion.answer !== this.value) {   
     }
     else{
-        //count as correct
-    }
-    Q++;
-    if(Q === listQuestions.length) {
-        console.log("end")
-    } else {
-    renderOptions()
-    }
-}
+    
+        function startTimer(){
+            console.log('timer suppose to go')
+            var timer = setInterval(function(){
+                sec--;
+                document.getElementById('timerDisplay').innerHTML='00:'+sec;
+                if (sec < 0) {
+                    clearInterval(timer);
+                    alert("Time is up!")
+                }
+            }, 1000);
+        }
+        document.getElementById('incorrect').addEventListener('click', function() {
+            sec -= 10;
+            document.getElementById('timerDisplay').innerHTML='00:'+sec;
+        });
+        startTimer();
+    })();
+
+   
+          
+    // if statement for right or wrong answers
+    
 /**
  * Listens for the user to click on the options div.
  * If the target of the click matches a <button> el
@@ -123,56 +144,3 @@ startButton.addEventListener("click", function () {
 
  
 });
-
-
-
-
-
-
-
-
-// GIVEN I am taking a code quiz
-// WHEN I click the start button
-// THEN a timer starts and I am presented with a question
-// WHEN I answer a question
-// THEN I am presented with another question
-// WHEN I answer a question incorrectly
-// THEN time is subtracted from the clock
-// WHEN all questions are answered or the timer reaches 0
-// THEN the game is over
-// WHEN the game is over
-// THEN I can save my initials and score
-
-//pseudo code
-
-// welcome screen w/ start button 
-// start button needs an event listener
-// there should be a function within the event listener for timer function
-// 1.timer starts a countdown (60 secs)
-// 2.prompt for a question with answer choices on it for user to select 
-// **easiest way to store info would be an array of objects!
-// within one object you will have 3 different keys:
-// 1. the question
-// 2.an array of answer choices
-// 3. a string with the correct answer
-// **the reason you want to group all of the above into one object is to show it in the screen at the same time
-// when you click the answer button it should spark another function: 
-// (Need an event listener to do this)
-// Function should:
-//  a. show the next question in the array of objects 
-//  b. show a message (text) saying if the answer is correct or incorrect 
-//  c. compare the text on the button to the correct answer choice to see if it is correct 
-//  d. if the text is incorrect, then decrement the timer
-// Once you are at the end of the questions or the timer is over then the game is over. Use the below to signal game over.
-// You will need a conditional statement: 
-// IF the timer is zero, THEN stop giving questions 
-// You will need your function to somehow be saved as a variable to stop the function. Similar to calling a function, you can stop.
-// Another screen should pop up at the end for high scores. 
-// 1.You need to have a way to save your initials and the timer score 
-// Put the information into local storage in order to save 
-
-
-
-
-
-
